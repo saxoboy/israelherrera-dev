f5faf98bb8405fd43c5019d0df49d6fb3d8da7bb
Isral Herrera <israelherrerae@gmail.com>
2023-05-26 17:16:16 -0500

    chore: config extras

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,8 @@ module.exports = {
   darkMode: 'class',
   theme: {
     fontFamily: {
-      body: ['Lato', 'Helvetica', 'Arial', 'sans-serif'],
-      display: ['Cinzel', 'Helvetica', 'sans-serif'],
+      body: ['Saira', 'Helvetica', 'Arial', 'sans-serif'],
+      display: ['Gruppo', 'Helvetica', 'sans-serif'],
     },
     screens: {
       sm: '640px',
@@ -36,55 +36,32 @@ module.exports = {
         900: '#4e505a',
       },
       primary: {
-        // Bondi Blue
-        50: '#ecfffe',
-        100: '#d0fdfb',
-        200: '#a6fbfa',
-        300: '#69f7f6',
-        400: '#24e8ec',
-        500: '#08ccd2',
-        default: '#0aa6b3', // Default
-        700: '#0f828f',
-        800: '#166874',
-        900: '#175762',
+        // polo-blue - azul
+        '50': '#f2f6fb',
+        '100': '#e7f0f8',
+        '200': '#d3e2f2',
+        '300': '#b8cee9',
+        '400': '#9bb4de',
+        '500': '#8fa4d6',
+        '600': '#697cc2',
+        '700': '#5868aa',
+        '800': '#4a5889',
+        '900': '#404b6f',
+        '950': '#262c40',
       },
       secondary: {
-        // Olive Green - verde
-        50: '#f8f7ed',
-        100: '#f0efd7',
-        200: '#e2e2b4',
-        300: '#ccce88',
-        default: '#b0b352', // default
-        500: '#999e44',
-        600: '#787d33',
-        700: '#5c612a',
-        800: '#4a4e26',
-        900: '#404324',
-      },
-      tertiary: {
-        // sunglo - rojo
-        50: '#fdf3f3',
-        100: '#fbe8e8',
-        200: '#f7d4d6',
-        300: '#f1b0b4',
-        default: '#e46f78', // default
-        500: '#db5865',
-        600: '#c6384d',
-        700: '#a62a40',
-        800: '#8c253b',
-        900: '#782337',
-      },
-      sanJuan: {
-        50: '#f4f6fb',
-        100: '#e9ecf5',
-        200: '#cdd7ea',
-        300: '#a1b4d8',
-        400: '#6f8ec1',
-        500: '#4d6faa',
-        600: '#3b578e',
-        default: '#334a7a', // Default
-        800: '#2b3d61',
-        900: '#283552',
+        //'gorse': {
+        '50': '#fefee8',
+        '100': '#ffffc2',
+        '200': '#fffa89',
+        '300': '#fff157',
+        '400': '#fde012',
+        '500': '#ecc506',
+        '600': '#cc9a02',
+        '700': '#a36e05',
+        '800': '#86560d',
+        '900': '#724611',
+        '950': '#432505',
       },
     },
     extend: {
