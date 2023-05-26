364111e8a592df5d0b5cdc1594d195633733be56
Isral Herrera <israelherrerae@gmail.com>
2023-05-26 17:15:27 -0500

    chore: add i18n

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,7 @@
  * @see https://github.com/iamvishnusankar/next-sitemap#readme
  */
 module.exports = {
-  siteUrl: 'http://localhost:3000', //https://www.isteps.edu.ec
+  siteUrl: 'http://localhost:3000', //https://www.israelherrera.dev
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
