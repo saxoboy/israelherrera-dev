83afb48604c63f7266b091fd7dc64b0ce5411a97
Isral Herrera <israelherrerae@gmail.com>
2023-06-13 11:10:06 -0500

    feat: add icons

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,7 @@
  * @see https://github.com/iamvishnusankar/next-sitemap#readme
  */
 module.exports = {
-  siteUrl: 'http://localhost:3000', //https://www.israelherrera.dev
+  siteUrl: 'https://www.israelherrera.dev',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
